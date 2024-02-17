65d6d640bf6076aa9d1e5ce60610570d07b9583c
Mihai <mihai_mm_home@outlook.com>
2024-02-17 21:36:00 +0200

    Background image; Removed empty elements from DIVs

--- js/chemistry.js
+++ js/chemistry.js
@@ -2566,3 +2566,8 @@ for (let i=0;i<118;i++) {
      } else {};
     
 }
+
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
